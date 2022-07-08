52e22bbaf210bd3dedebd803cf200a9f91a56b78
piotr <piotrsabatura@gmail.com>
2022-07-08 19:04:12 +0200

    indexView and template update

--- order_management/order_app/views.py
+++ order_management/order_app/views.py
@@ -3,6 +3,34 @@ from .models import *
 
 # Create your views here.
 def indexView(request):
-    context = {}
-    return render(request, 'order_app/index.html', context)
+    # show amount of orders:
+    orders = Order.objects.all()
+    employees = Employee.objects.all()
+
+    total_employees = employees.count()
+    total_orders = orders.count()
+    STATUS = (
+        ('W trakcie zamówienia', 'W trakcie zamówienia'),
+        ('Zamówiono', 'Zamówiono'),
+        ('Dostarczono', 'Dostarczono'),
+    )
+    pending = orders.filter(status='W trakcie zamówienia').count()
+    ordered = orders.filter(status='Zamówione')
+    delivered = orders.filter(status='Dostarczone')
+
+    context = {'orders': orders,
+               'total_employees': total_employees,
+               'total_orders': total_orders,
+               'pending': pending,
+               'ordered': ordered,
+               'delivered': delivered,
+               }
+    template = 'order_app/index.html'
+
+    return render(request, template, context)
+
+
+
+
+
 
